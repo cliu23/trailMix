9eb4fab16b53980d3eea2f2e5aa01f14aad5b78c
Calvin <calvinliu24@gmail.com>
2015-06-03 19:04:26 -0700

    added in javascript and quotes

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module TrailMix
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+config.assets.enabled = true
+endconfig.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -33,3 +34,5 @@ module TrailMix
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+
